069d951b94034d56af4c1f9ad7c157fc542cf979
Moses <b8002246@hallam.shu.ac.uk>
2021-05-06 11:22:53 +0100

    Add project files.

---
+++ CarRentalManagement2/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CarRentalManagement2.Server.Data;
+using CarRentalManagement2.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarRentalManagement2.Server.Areas.Identity.IdentityHostingStartup))]
+namespace CarRentalManagement2.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
